031ed41c2a59bbaa0f7786c1c0ecddc471c1014b
Horváth Zsombor Ákos <horvath.zsombor.akos@uni-obuda.hu>
2025-12-15 09:47:00 +0100

    Kezdő projekt létrehozása a repository-ban

---
+++ Git_MVC_App_Pelda/Git_MVC_App_Pelda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Git_MVC_App_Pelda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
